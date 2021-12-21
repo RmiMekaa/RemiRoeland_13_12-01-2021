abbf987de82ad64c0fbfcaafede024a361780a76
Rémi <roeland.remi@yahoo.fr>
2021-12-21 13:38:17 +0100

    Create Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+/**
+ * React component for the footer
+ * @component 
+ */
+export function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Copyright 2020 Argent Bank</p>
+    </footer>
+  )
+}
