af9105146b11f8ae12b64a0590e61247800f953f
Rémi <roeland.remi@yahoo.fr>
2022-01-12 15:04:49 +0100

    Create mocked accounts data for transactions page

---
+++ src/mocks/accounts.js
@@ -0,0 +1,146 @@
+export const accountsMockData = [
+  {
+    id: "Argent Bank Checking (x8349)",
+    name: "Argent Bank Checking",
+    amount: "2,082.79",
+    balance: "Available Balance",
+    transactions: [
+      {
+        id: "893542268",
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "5.0",
+        balance: "2082.79",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "9517786353",
+        date: "June 19th, 2020",
+        description: "Irish Pub",
+        amount: "4.5",
+        balance: "2087.79",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "7899683933",
+        date: "June 18th, 2020",
+        description: "Chez Gusteau",
+        amount: "54.0",
+        balance: "2092.29",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "3861957346",
+        date: "June 17th, 2020",
+        description: "SNCF",
+        amount: "37.0",
+        balance: "2146,29",
+        type: "Electronic",
+        category: "Transports",
+        notes: "..."
+      },
+      {
+        id: "5182346780",
+        date: "June 16th, 2020",
+        description: "Dr Strange",
+        amount: "26.0",
+        balance: "2183,29",
+        type: "Electronic",
+        category: "Medical",
+        notes: "..."
+      }
+    ]
+  },
+  {
+    id: "Argent Bank Savings (x6712)",
+    name: "Argent Bank Savings",
+    amount: "10,928.42",
+    balance: "Available Balance",
+    transactions: [
+      {
+        id: "893542268",
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "5.0",
+        balance: "2082.79",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "9517786353",
+        date: "June 19th, 2020",
+        description: "Irish Pub",
+        amount: "4.5",
+        balance: "2087.79",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "7899683933",
+        date: "June 18th, 2020",
+        description: "Chez Gusteau",
+        amount: "54.0",
+        balance: "2092.29",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+    ]
+  },
+  {
+    id: "Argent Bank Credit Card (x8349)",
+    name: "Argent Bank Credit Card",
+    amount: "184.30",
+    balance: "Current Balance",
+    transactions: [
+      {
+        id: "893542268",
+        date: "June 20th, 2020",
+        description: "Golden Sun Bakery",
+        amount: "5.0",
+        balance: "2082.79",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "9517786353",
+        date: "June 19th, 2020",
+        description: "Irish Pub",
+        amount: "4.5",
+        balance: "2087.79",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "7899683933",
+        date: "June 18th, 2020",
+        description: "Chez Gusteau",
+        amount: "54.0",
+        balance: "2092.29",
+        type: "Electronic",
+        category: "Food",
+        notes: "..."
+      },
+      {
+        id: "3861957346",
+        date: "June 17th, 2020",
+        description: "SNCF",
+        amount: "37.0",
+        balance: "2146,29",
+        type: "Electronic",
+        category: "Transports",
+        notes: "..."
+      },
+    ]
+  }
+]
