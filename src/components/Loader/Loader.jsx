ada46a34d59a1f548508912760aaa4be677b4632
Rémi <roeland.remi@yahoo.fr>
2022-01-14 16:16:32 +0100

    Creation of a loadin spinner component

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function Loader() {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
