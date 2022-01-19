811e8c21ddbd5514d817e65056e5bebcd4afe288
Rémi <roeland.remi@yahoo.fr>
2022-01-19 20:30:12 +0100

    Creation of selectors.js

---
+++ src/store/selectors.js
@@ -0,0 +1,8 @@
+//Here you'll find all selectors used in the app
+
+export const selectUserFirstName = (state) => state.user.firstName;
+export const selectUserLastName = (state) => state.user.lastName;
+export const selectAuthLoggedIn = (state) => state.auth.loggedIn;
+export const selectAuthError = (state) => state.auth.error;
+export const selectAuthLoading = (state) => state.auth.loading;
+export const selectAuthToken = (state) => state.auth.token;
