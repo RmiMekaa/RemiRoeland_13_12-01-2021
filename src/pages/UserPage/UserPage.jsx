62cb2c9d3251ad147a1dbf5dc6136456b01ce165
Rémi <roeland.remi@yahoo.fr>
2022-01-24 16:24:33 +0100

    Add UseEffect to listen for token expiration

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { UserHeader } from "../../components/UserHeader/UserHeader";
 import { Account } from "../../components/Account/Account";
 import { accountsMockData } from "../../mocks/accounts";
-import { selectAuthLoggedIn, selectUserError, selectUserLoading } from "../../store/selectors";
+import { selectAuthLoggedIn, selectAuthToken, selectUserError, selectUserLoading } from "../../store/selectors";
 import { Loader } from "../../components/Loader/Loader";
 import { Navigate } from "react-router-dom";
+import jwtDecode from "jwt-decode";
+import { useDispatch } from "react-redux";
 
 export function UserPage() {
   const isLoggedIn = useSelector(selectAuthLoggedIn)
+  const token = useSelector(selectAuthToken);
   const fetchingProfile = useSelector(selectUserLoading)
   const fetchingProfileError = useSelector(selectUserError)
   const accounts = accountsMockData; //Accounts data mock
+  const dispatch = useDispatch()
+
+  //As this page is a private route, we have to check the token expiration date and disconnect the user if it's expired
+  useEffect(() => {
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        dispatch({ type: 'DISCONNECT' });
+      }
+    }
+  }, [dispatch, token])
 
   if (!isLoggedIn) { return <Navigate to='/login' /> }
 
@@ -23,21 +37,20 @@ export function UserPage() {
         <h2>Oops...</h2>
         <p>
           Failed to retrieve profile, please try again later <br />
-          <span>Error: {fetchingProfileError.message}</span>
+          <span>Error: {fetchingProfileError.data.message}</span>
         </p>
       </div>
     )
   }
 
-  if (isLoggedIn) {
-    return (
-      <main className="main bg-dark">
-        <h2 className="sr-only">Accounts</h2>
-        <UserHeader />
-        {accounts.map((account) => (
-          <Account key={account.id + account.name} data={account} />
-        ))}
-      </main>
-    )
-  }
+  return (
+    <main className="main bg-dark">
+      <h2 className="sr-only">Accounts</h2>
+      <UserHeader />
+      {accounts.map((account) => (
+        <Account key={account.id + account.name} data={account} />
+      ))}
+    </main>
+  )
+
 }
