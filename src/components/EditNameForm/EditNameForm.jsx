486a29c7042be581ca177cc74a3845654bd60870
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-03-02 17:52:25 +0100

    Refactorisation - add edit name loader

--- src/components/EditNameForm/EditNameForm.jsx
+++ src/components/EditNameForm/EditNameForm.jsx
@@ -1,10 +1,10 @@
 import React from "react"
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
-import { selectUserFirstName, selectUserLastName } from "../../store/selectors";
+import { selectUser } from "../../store/selectors";
 import { useState } from "react";
-import { sendEditNameRequest } from "../../services/api";
-import { editNameAction } from "../../store/actions/UserActions";
+import { editName } from "../../store/actions/UserActions";
+import { Loader } from "../Loader/Loader";
 
 /**
  * React component for the edit name form
@@ -14,19 +14,26 @@ import { editNameAction } from "../../store/actions/UserActions";
  */
 export function EditNameForm(props) {
   const { setEdit } = props;
-  const firstName = useSelector(selectUserFirstName);
-  const lastName = useSelector(selectUserLastName)
-  const dispatch = useDispatch()
+  const userState = useSelector(selectUser);
+  const { firstName, lastName, loading, error } = userState;
+  const dispatch = useDispatch();
 
   const [firstNameInputValue, setNewFirstNameInputValue] = useState(firstName);
   const [lastNameInputValue, setNewLastNameInputValue] = useState(lastName);
   const [displayError, setDisplayError] = useState(false);
   const [errorMsg, setErrorMsg] = useState("");
 
+  if (loading || error) {
+    return (
+      <div className="editNameForm">
+        {loading ? <Loader /> : <span>{error}</span>}
+      </div>
+    )
+  }
 
   return (
     <div className="editNameForm">
-      {displayError ? (<span style={{ color: "red" }}>{errorMsg}</span>) : null}
+      {displayError ? (<span className="errorMsg">{errorMsg}</span>) : null}
       <div className="editNameForm__inputs">
         <input className="editNameForm__inputs__input" placeholder={firstName} onChange={(e) => setNewFirstNameInputValue(e.target.value)} />
         <input className="editNameForm__inputs__input" placeholder={lastName} onChange={(e) => setNewLastNameInputValue(e.target.value)} />
@@ -39,27 +46,17 @@ export function EditNameForm(props) {
   )
 
   /**
-   * 1- Capitalize firstName and lastName
-   * 2- Send the edit name request
-   * 3- If request is successful, dispatch the edit name action
-   * @returns {Promise}
+   * Handle form submission
+   * If form is valid, dispatch the editName action
+   * @return {void}
    */
-  async function handleSubmit() {
+  function handleSubmit() {
     let isValid = formValidation();
     if (isValid) {
       let newFirstName = firstNameInputValue[0].toUpperCase() + firstNameInputValue.substring(1);
       let newLastName = lastNameInputValue[0].toUpperCase() + lastNameInputValue.substring(1);
-      let response = await sendEditNameRequest(newFirstName, newLastName)
-      if (response.status === 200) {
-        const payload = { newFirstName, newLastName }
-        dispatch(editNameAction(payload))
-        setEdit(false)
-      }
-      else {
-        setDisplayError(true)
-        let message = "Sorry, you are not allowed to do this. Error: " + response.data.message;
-        setErrorMsg(message)
-      }
+      dispatch(editName(newFirstName, newLastName))
+      setEdit(false)
     }
   }
 
@@ -70,13 +67,10 @@ export function EditNameForm(props) {
    */
   function formValidation() {
     let regex = /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð,.'-]+$/u;
-    if (firstNameInputValue.match(regex) && lastNameInputValue.match(regex)) {
-      return true;
-    }
-    else if (!firstNameInputValue || !lastNameInputValue) { setErrorMsg("Veuillez renseigner tous les champs") }
-    else { setErrorMsg("Les champs ne peuvent contenir que des lettres") }
+    if (firstNameInputValue.match(regex) && lastNameInputValue.match(regex)) return true;
+    if (!firstNameInputValue || !lastNameInputValue) setErrorMsg("Veuillez renseigner tous les champs")
+    else setErrorMsg("Les champs ne peuvent contenir que des lettres")
     setDisplayError(true);
     return false;
   }
-
 }
