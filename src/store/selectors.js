8ef8003eb184460f1c8ec18309d688428b6c5781
Rémi Roeland,nhh,,,yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyh <roeland.remi@yahoo.fr>
2022-03-07 12:36:54 +0100

    add selectors for edit name form

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -1,10 +1,14 @@
 //Here you'll find all selectors used in the app
 
+// //User Slice
 export const selectUserLoading = (state) => state.user.loading;
 export const selectUserError = (state) => state.user.error;
+export const selectEditError = (state) => state.user.editError;
+export const selectDisplayEditForm = (state) => state.user.displayEditForm;
 export const selectUserFirstName = (state) => state.user.firstName;
 export const selectUserLastName = (state) => state.user.lastName;
 
+// //Auth Slice
 export const selectAuthLoggedIn = (state) => state.auth.loggedIn;
 export const selectAuthError = (state) => state.auth.error;
 export const selectAuthLoading = (state) => state.auth.loading;
